3fc4983c92efa137bf1ae0ed2ce93371c849e9c5
Jairo Gatjens <jgatjens@gmail.com>
2014-09-26 21:13:57 -0600

    ADD - NProgress bar to ui-router and XMLHttpRequest Object

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,8 +39,6 @@ angular.module('ngVet', [
     Parse.initialize('8WUpXyrczIHufksKvskQ8hrA3eiroUWhvnguAW8l', 'tIDTd21YkMetRjQFLwY29GW5zcKBzgk5DoX6gUEj');
 
     // Init Facebook
-    // 3) Finally, init Facebook
-
     window.fbAsyncInit = function() {
       Parse.FacebookUtils.init({
         appId: '1544086335825154',
@@ -52,6 +50,7 @@ angular.module('ngVet', [
 
     };
 
+    // Add facebook sdk to the document
     (function(d, s, id){
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) {return;}
@@ -60,13 +59,64 @@ angular.module('ngVet', [
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
+
+    // Modified XMLHttpRequest to add a listener for start and stop the progress bar
+    //
+    // Save the real open
+    var oldOpen = XMLHttpRequest.prototype.open;
+
+    function onStateChange() {
+
+      // fires on every readystatechange ever
+      // use `this` to determine which XHR object fired the change event
+      if (this.readyState === 1) {
+        NProgress.start();
+      }
+
+      if (this.readyState === 4 && this.status === 200) {
+        NProgress.done(true);
+      }
+
+      // Add red color
+      if (this.readyState === 4 && this.status === 404) {
+          var NProgressDOM =  document.getElementById('nprogress');
+          NProgressDOM.className = 'fail';
+
+          NProgress.inc(60);
+
+          setTimeout(function() {
+            NProgress.done();
+          }, 500);
+
+          setTimeout(function() {
+            NProgressDOM.className = '';
+          }, 1500);
+      }
+    }
+
+    XMLHttpRequest.prototype.open = function() {
+        // when an XHR object is opened, add a listener for its readystatechange events
+        this.addEventListener('readystatechange', onStateChange);
+        // this.addEventListener("load", onLoadSuccess);
+        // run the real `open`
+        oldOpen.apply(this, arguments);
+    };
+
+
   })
 
   .run(function( $rootScope, profile, $state) {
 
 
+    $rootScope.$on('$stateChangeStart', function(){
+      // start
+      NProgress.start();
+    });
+
     // $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
     $rootScope.$on('$stateChangeSuccess',function(event, toState){
+
+      NProgress.done();
 
       // check for child states
       if (toState.name.match(/\./g)) {
