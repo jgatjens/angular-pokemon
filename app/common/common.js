8c1a94ee818a7ff8936224b0624ce40f7ab4a728
Jairo Gatjens <jgatjens@gmail.com>
2014-10-07 09:15:33 -0600

    add - scaffolding to handle angular filters

--- app/common/common.js
+++ app/common/common.js
@@ -3,5 +3,6 @@
 angular.module('ngVet.common', [
   'ngVet.common.directives',
   'ngVet.common.factories',
-  'ngVet.common.services'
+  'ngVet.common.services',
+  'ngVet.common.filters'
 ]);
---
+++ app/common/filters/_filters.js
@@ -0,0 +1,6 @@
+
+'use strict';
+
+angular.module('ngVet.common.filters', [
+  'ngVet.common.filters.timeago'
+]);
