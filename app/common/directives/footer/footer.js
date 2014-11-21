41558a116c06e420ef797f931a5fdbf55380a82a
Jairo Gatjens <jgatjens@gmail.com>
2014-11-20 19:14:04 -0600

    update app dependencies via bower

--- app/common/directives/footer/footer.js
+++ app/common/directives/footer/footer.js
@@ -6,6 +6,16 @@ angular.module('ngVet.common.directives.footer', [])
     return {
       restrict    : 'EA',
       replace     : true,
+      controller  : 'FooterCtrl',
       templateUrl : 'common/directives/footer/footer.tpl.html'
     }
+  })
+
+  .controller('FooterCtrl', function ($scope, $window) {
+
+    $scope.openTwitter = function(e) {
+      // e.preventDefault();
+      $window.open('https://twitter.com/jgatjens');
+    }
+
   });
